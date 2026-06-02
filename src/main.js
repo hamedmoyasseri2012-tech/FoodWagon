1863d1044cf0b2c95521bf3c33356100928e8f21
milan1990 <hamed.moyasseri2012@gmail.com>
2026-06-03 02:45:00 +0330

    v11

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-const firstList = document.getElementById("listFood")
-const rightArrow = document.getElementById("rightarrow")
-const leftArrow = document.getElementById("leftaarow")
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
 
+import "swiper/css";
+import "swiper/css/navigation";
 
+const firstList = document.getElementById("listFood");
+const rightArrow = document.getElementById("rightarrow");
+const leftArrow = document.getElementById("leftaarow");
 
-const secondList = document.getElementById("ListFood2")
-const rightArrow1 = document.getElementById("rightarrow2")
+const secondList = document.getElementById("ListFood2");
+const rightArrow1 = document.getElementById("rightarrow2");
 const leftArrow1 = document.getElementById("leftarrow2");
