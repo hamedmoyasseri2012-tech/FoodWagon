6a401b65fb0b937481defae63d971acae11313c4
milan1990 <hamed.moyasseri2012@gmail.com>
2026-06-03 02:14:39 +0330

    v10

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const firstList = document.getElementById("listFood")
+const rightArrow = document.getElementById("rightarrow")
+const leftArrow = document.getElementById("leftaarow")
+
+
+
+const secondList = document.getElementById("ListFood2")
+const rightArrow1 = document.getElementById("rightarrow2")
+const leftArrow1 = document.getElementById("leftarrow2");
